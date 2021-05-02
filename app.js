7154171e5aba24a1a4c5a6d6365765b4f93815ae
zeinab1233 <zeinab.albadareen@gmail.com>
2021-05-02 15:04:06 +0300

    multipl

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a,b) { 
 let sumResult = a + b;
-return [sumResult,`the sum of ${a} and ${b} is ${sumResult}.`];
+return [sumResult,`The sum of ${a} and ${b} is ${sumResult}.`];
 }
 
 
@@ -28,12 +28,13 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function multiply(a, b) {
+     let multresult = a * b;
+     return[multresult,`The product of ${a} and ${b} is ${multresult}.`]
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
