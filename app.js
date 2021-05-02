e4682ffaf1c44e4e6c3b554dc24de3385b490a97
zeinab1233 <zeinab.albadareen@gmail.com>
2021-05-03 02:23:37 +0300

    prob2,3

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a,b) { 
-let sumResult = a + b;
-return [sumResult,`The sum of ${a} and ${b} is ${sumResult}.`];
-}
+     let sumResult = a + b;
+     return [sumResult,`The sum of ${a} and ${b} is ${sumResult}.`];
+     }
+     
+     
+     // Here is the test for sum(); uncomment it to run it
+     testSum(4, 7);
+     
 
 
 // Here is the test for sum(); uncomment it to run it
@@ -30,7 +35,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) {
      let multresult = a * b;
-     return[multresult,`The product of ${a} and ${b} is ${multresult}.`]
+     return [multresult,`The product of ${a} and ${b} is ${multresult}.`];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -50,12 +55,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sumAndMultiply(a, b, c) { 
+     let d= testSum(a,b);  
+     let e= testSum(d,c);
+     let f = testMultiply(a,b); 
+     let g = testMultiply(f,c);
+     
+     return [e, g, `${a} and ${b} and ${c} sum to ${e}.`, `the product of ${a} and ${b} and ${c} is ${g}.`]; 
+    
+
+    
 
 }
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+// // Here is the test for sumAndMultiply(); uncomment it to run it
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
